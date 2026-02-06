3f53b71d2ff587aa6bcf22c1f921ff1ebe5ffbed
choidaye8170 <gracechoidaye@gmail.com>
2026-02-06 23:30:47 +0900

    feat : 디자인 시스템 설정(색상)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 export default function HomePage() {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center gap-6 px-10">
-      <div className="typo-pretitle1k text-purple-500">HomePage</div>
+      <div className="typo-pretitle1k text-primary-brand">HomePage</div>
 
       <nav className="flex flex-col gap-4">
         {["P A R T", "A B O U T U S", "P R O J E C T"].map((item) => (
           <button
             key={item}
-            className="typo-pretitle2e text-black hover:text-purple-500 transition underline underline-offset-8 decoration-transparent hover:decoration-purple-500"
+            className="typo-pretitle2e text-line hover:text-text transition underline underline-offset-8 decoration-transparent hover:decoration-text"
           >
             {item}
           </button>
