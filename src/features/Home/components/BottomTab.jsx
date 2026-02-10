fa9134a191dcd75729c2de5fa23e7286c54b01c2
choidaye8170 <gracechoidaye@gmail.com>
2026-02-11 03:18:18 +0900

    feat : BottomTab에 /part 이동 추가

--- src/features/Home/components/BottomTab.jsx
+++ src/features/Home/components/BottomTab.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-export default function BottomTab({ text }) {
+export default function BottomTab({ text, onClick }) {
   return (
-    <div className="flex w-full h-[8.1875rem] p-[0.625rem] justify-center items-start gap-[0.625rem] border-line border-[0.5px]">
+    <div
+      onClick={onClick}
+      className="flex w-full h-[8.1875rem] p-[0.625rem] justify-center items-start gap-[0.625rem] border-line border-[0.5px] cursor-pointer"
+    >
       <p className="typo-bottomtab">{text}</p>
     </div>
   );
--- src/features/Home/components/BottomTabBar.jsx
+++ src/features/Home/components/BottomTabBar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import BottomTab from "./BottomTab";
 
 export default function BottomTabbar() {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full bg-white flex">
       <BottomTab text="ABOUT US" />
-      <BottomTab text="PART" />
+      <BottomTab
+        text="PART"
+        onClick={() => navigate("/part")}
+      />
       <BottomTab text="PROJECT" />
     </div>
   );
