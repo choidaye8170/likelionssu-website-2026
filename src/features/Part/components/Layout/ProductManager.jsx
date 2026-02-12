f9b6babccb082f697e20a848cde6432f7917d7db
choidaye8170 <gracechoidaye@gmail.com>
2026-02-13 02:55:35 +0900

    feat : PmContent 생성

--- src/features/Part/components/Layout/ProductManager.jsx
+++ src/features/Part/components/Layout/ProductManager.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import PmIntro from '../Pm/PmIntro'
 import Lineup from '../Lineup'
 import PmContent from '../Pm/PmContent'
+import SessionSection from '../SessionSection'
 
 export default function ProductManager() {
   return (
     <div className='flex flex-col items-center w-full px-[1.19rem] sm:px-0'>
-      <div className='pb-[3.75rem] sm:pb-[3.87rem]'>
+      <div className='pb-[3.375rem] sm:pb-[3.87rem]'>
         <PmIntro/>
         <Lineup/>
       </div>
-      <div>
+      <div className='w-full pt-[3.375rem] sm:pt-[0rem] pb-[3.375rem] sm:pb-[0rem]'>
         <PmContent/>
+      </div>
+      <div className='w-full'>
+        <SessionSection/>
       </div>
     </div>
   )
---
+++ src/features/Part/components/SessionSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SessionSection() {
+  return (
+    <div className='flex flex-col items-center bg-line pt-[3.375rem] sm:pt-[6.81rem]'>
+        <div>session</div>
+    </div>
+  )
+}
