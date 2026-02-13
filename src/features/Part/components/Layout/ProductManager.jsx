b8e2b886bc31176e7372691ba6e0b133a3a66002
choidaye8170 <gracechoidaye@gmail.com>
2026-02-14 03:12:37 +0900

    feat : ShortcutBtn에 up 추가

--- src/features/Part/components/Layout/ProductManager.jsx
+++ src/features/Part/components/Layout/ProductManager.jsx
@@ -3,6 +3,7 @@ import PmIntro from "../Pm/PmIntro";
 import Lineup from "../Lineup";
 import PmContent from "../Pm/PmContent";
 import SessionSection from "../SessionSection";
+import ShortcutBtn from "../ShortcutBtn";
 
 export default function ProductManager() {
   return (
@@ -19,7 +20,16 @@ export default function ProductManager() {
       <div className="w-full sm:w-[63.4375rem]">
         <SessionSection />
       </div>
+
+      <div className="mb-[0.81] sm:mb-[1.37rem] flex flex-col items-end sm:flex-row sm:justify-between sm:items-end w-full mt-[6.19rem] px-[1.25rem] sm:px-0 sm:mt-[10.69rem] sm:w-[75.8125rem]">
+      <p className="hidden sm:block typo-footer-custom text-text">
+        Beautiful moments we will create together
+      </p>
+        <ShortcutBtn text="맨 위로" direction="up" />
+      <p className="sm:hidden w-full mt-[1.88rem] typo-footer-custom text-text">
+        Beautiful moments we will create together
+      </p>
+      </div>
     </div>
   );
 }
-
--- src/features/Part/components/ShortcutBtn.jsx
+++ src/features/Part/components/ShortcutBtn.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import down from "../assets/down.svg";
+import up from "../assets/up.svg";
 
-export default function ShortcutBtn({ text = "파트장의 말" }) {
+export default function ShortcutBtn({ text = "파트장의 말", direction = "down" }) {
+  const icon = direction === "up" ? up : down;
+
   return (
     <div className="group flex border-[0.7px] border-text w-[7.625rem] gap-[0.3125rem] justify-center items-center p-[0.625rem]">
       <p className="typo-small1 text-text">{text}</p>
 
       <img
-        src={down}
-        alt="down"
+        src={icon}
+        alt={direction}
         className="w-[0.625rem] h-[0.6875rem] opacity-0 group-hover:opacity-100 transition-opacity duration-200"
       />
     </div>
   );
 }
 
-//<ShortcutBtn text="세션 소개" />
+{/* <ShortcutBtn text="세션 소개" direction="up" />
+<ShortcutBtn text="파트장의 말" direction="down" /> */}
