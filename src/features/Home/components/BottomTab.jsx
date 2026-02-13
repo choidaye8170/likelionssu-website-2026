a0ae2f177bf2eae0c043d180f79825e11d1fbc0d
choidaye8170 <gracechoidaye@gmail.com>
2026-02-14 04:14:42 +0900

    feat : BottomTab 호버효과 설정

--- src/features/Home/components/BottomTab.jsx
+++ src/features/Home/components/BottomTab.jsx
@@ -4,9 +4,12 @@ export default function BottomTab({ text, onClick }) {
   return (
     <div
       onClick={onClick}
-      className="flex w-full h-[8.1875rem] p-[0.625rem] justify-center items-start gap-[0.625rem] border-line border-[0.5px] cursor-pointer"
+      className="group flex w-full h-[8.1875rem] p-[0.625rem] justify-center items-start gap-[0.625rem] border-line border-[0.5px] cursor-pointer 
+      hover:bg-secondarybrand transition-colors duration-200"
     >
-      <p className="typo-bottomtab">{text}</p>
+    <p className="typo-bottomtab !text-text group-hover:!text-primarybrand transition-colors duration-200">
+      {text}
+    </p>
     </div>
   );
 }
