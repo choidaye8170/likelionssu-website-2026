9e1b4006e3ca386cf6397ed2347d22e6a15d4476
ukyeong-user <hyk040520@naver.com>
2026-02-15 22:31:14 +0900

    feat: ClosingSection 추가 및 모바일 오른쪽 여백 제거

--- src/pages/RecruitPage.jsx
+++ src/pages/RecruitPage.jsx
@@ -9,6 +9,7 @@ import PartSection from "../features/Recruit/components/PartSection";
 import RoadmapSection from "../features/Recruit/components/RoadmapSection";
 import TimelineSection from "../features/Recruit/components/TimelineSection";
 import FaqSection from "../features/Recruit/components/FaqSection";
+import ClosingSection from "../features/Recruit/components/ClosingSection";
 
 export default function RecruitPage() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -24,17 +25,20 @@ export default function RecruitPage() {
         onMenuClick={toggleSidebar}
       />
 
-      <div className="pt-[5.9375rem] sm:pt-0">
-        <IntroSection />
-      </div>
+      <div className="overflow-x-hidden">
+        <div className="pt-[5.9375rem] sm:pt-0">
+          <IntroSection />
+        </div>
 
-      <div className="animate-recruit-content-enter">
-        <ValueSection />
-        <PartSection />
-        <RoadmapSection />
-        <TimelineSection />
-        <FaqSection />
-        <Footer />
+        <div className="animate-recruit-content-enter">
+          <ValueSection />
+          <PartSection />
+          <RoadmapSection />
+          <TimelineSection />
+          <FaqSection />
+          <ClosingSection />
+          <Footer />
+        </div>
       </div>
       <SideBar isOpen={isSidebarOpen} onClose={closeSidebar} />
     </div>
