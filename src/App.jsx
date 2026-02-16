77d838b1b6616f1d8319e4d1a30dbd94fadd2147
sunwin1029 <sunwin1029@gmail.com>
2026-02-16 18:08:01 +0900

    포커스 문제 / 사이드바 라우팅 문제 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { Outlet } from "react-router-dom"
+import { useLayoutEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+
+function RouteScrollManager() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  useLayoutEffect(() => {
+    if (!("scrollRestoration" in window.history)) return undefined;
+
+    const previous = window.history.scrollRestoration;
+    window.history.scrollRestoration = "manual";
+
+    return () => {
+      window.history.scrollRestoration = previous;
+    };
+  }, []);
+
+  return null;
+}
 
 function App() {
   return (
     <>
-      <Outlet/>
+      <RouteScrollManager />
+      <Outlet />
     </>
-  )
+  );
 }
 
 export default App;
