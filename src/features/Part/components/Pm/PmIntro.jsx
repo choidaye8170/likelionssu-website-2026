a2ce8decc28875891ad4ce3a442dc164f971ade3
choidaye8170 <gracechoidaye@gmail.com>
2026-02-19 13:29:14 +0900

    design : part 페이지 모바일 수정 및 be 텍스트 들여쓰기 수정

--- src/features/Part/components/Pm/PmIntro.jsx
+++ src/features/Part/components/Pm/PmIntro.jsx
@@ -20,7 +20,7 @@ About the PM Part`}
           </span>
         </div>
 
-        <div className="w-[35.1875rem] typo-small2 text-text sm:hidden">
+        <div className="w-full typo-small2 text-text sm:hidden">
           PM(Product-Manager)은 프로덕트의 전반적인 흐름을 관리하며, 목표 달성을 위한 전략적 방향과
           우선순위를 결정합니다. 프로덕트의 목표와 기준을 명확히 설정하고, 팀이 같은 방향으로
           나아갈 수 있도록 핵심 의사결정을 주도합니다. 개발 진행 중 발생하는 이슈와 리스크를
